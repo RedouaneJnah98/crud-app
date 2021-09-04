a1fc0f14afa15ed9b2b5bf2b74ee849e27ab7bf9
RedouaneJnah98 <redouanejnah2000@gmail.com>
2021-09-04 17:38:31 +0100

    setup sass files

--- index.php
+++ index.php
@@ -1,6 +1,47 @@
 <?php include_once "includes/header.php"; ?>
 
-<h1>Hello world</h1>
+<section class="container">
+    <img src="./images/login-img.jpg" class="login-img" alt="technology image">
+
+    <article class="login-section">
+        <h2 class="title">Register</h2>
+        <h4>manage all your lottery efficiently</h4>
+        <p>let's get you all set up you can verify your personal account and begin setting up your profile.</p>
+
+        <!-- form -->
+        <form action="" method="post">
+
+            <div class="form-group">
+                <div>
+                    <label for="firstname">First Name</label>
+                    <input type="text" name="first_name">
+                </div>
+                <div>
+                    <label for="lastname">Last Name</label>
+                    <input type="text" name="last_name">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="phone">Phone Number</label>
+                <input type="number" name="phone_number">
+                <label for="email">Email</label>
+                <input type="email" name="email">
+            </div>
+
+            <div class="form-group">
+                <label for="user_password">Password</label>
+                <input type="password" name="user_password">
+                <label for="confirmPassword">Confirm Passord</label>
+                <input type="password" name="confirm_password">
+            </div>
+
+        </form>
+        <!-- end of form -->
+    </article>
+</section>
+
+
 </body>
 
 </html>
